957bbb3d7c2d59727261dfc206c51bfa3aea8a9e
canwaykalburim <canwaykalburim@gmail.com>
2019-08-13 19:01:16 +0900

    클리커 게임 리메이크

---
+++ Clicker Game Ver.2/Assets/Scripts/ClickButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickButton : MonoBehaviour
+{
+    public int gold = 0;
+    public int goldPerClick = 1;
+
+    public void OnClick()
+    {
+        gold += goldPerClick;
+    }
+}
